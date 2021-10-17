b473d9328b0f89aac0f663b1fa58ef324dfd16d3
GPrathap <ggeesara@gmail.com>
2021-10-17 14:16:54 +0300

    adding GPI rendering

--- cpu/run.sh
+++ cpu/run.sh
@@ -1,8 +1,8 @@
 docker stop cognitive_robotics_cpu
 docker rm cognitive_robotics_cpu
-docker run -it \
-  -p 6080:6080 \
-  -p 8888:8888 \
-  --name cognitive_robotics_cpu \
-  --mount source=cognitive-robotics-opt-volume,target=/opt \
-  prathap/cognitive_robotics_cpu:v1
+docker run -it -d --privileged  -e DISPLAY=$DISPLAY \
+                --mount source=cognitive-robotics-opt-volume,target=/opt  \
+                -v /tmp/.X11-unix:/tmp/.X11-unix:rw -p 6080:6080 -p 8888:8888 \
+                --name cognitive_robotics_cpu prathap/cognitive_robotics_cpu:v1 bash
+xhost +
+docker exec -it cognitive_robotics_cpu bash
---
+++ gpu/attach.sh
@@ -0,0 +1,2 @@
+docker exec -it cognitive_robotics_gpu bash
+
